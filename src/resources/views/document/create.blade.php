383235c166d9eeb62d1134e7f9709e0fa0e56ecf
Marius <marius.staud@icloud.com>
2021-02-09 12:24:18 +0100

    Home angepasst

--- src/resources/views/document/create.blade.php
+++ src/resources/views/document/create.blade.php
@@ -2,17 +2,16 @@
 
 @section('content')
 
-
-<div class= "col-md-9 col-lg-9 pull-right"></div>
-<h2>Neue Datei hochladen</h2>
-<p class="sub">Laden Sie hier eine neue Datei mit Dateiname und Beschreibung hoch.</p>
-<form action="/files" method="POST" enctype="multipart/form-data">
-    {{csrf_field()}}
-    <input type="text" name="title" placeholder="Dateiname">
-    <input type="text" name="description" placeholder="Beschreibung" size="55">
-    <input type="file" name="file" style="padding: 2em 1em;">
-    <br>
-    <button type="submit" class="btn btn-dark" value="Hochladen">Hochladen</button>
-</form>
+<div class= "col-md-9 col-lg-9 pull-right">
+    <h2>Neue Datei hochladen</h2>
+    <p class="sub">Laden Sie hier eine neue Datei mit Dateiname und Beschreibung hoch.</p>
+    <form action="/files" method="POST" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="text" name="title" placeholder="Dateiname">
+        <input type="text" name="description" placeholder="Beschreibung" size="55">
+        <input type="file" name="file" style="padding: 2em 1em;">
+        <br>
+        <button type="submit" class="btn btn-dark" value="Hochladen">Hochladen</button>
+    </form>
 </div>
 @endsection
--- src/resources/views/document/details.blade.php
+++ src/resources/views/document/details.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 <div class= "col-md-9 col-lg-9 pull-right">
-<h2>{{$data->title}}</h2>
-<p>{{$data->description}}</p>
-<p>
-    <iframe src="{{url('storage/' .$data->file)}}" style="width: 600px;
-    height: 500px;"></iframe>
-</p>
-<button class="btn btn-dark"><a href="/files" style="color: white">Zurück</a></button>
+    <h2>{{$data->title}}</h2>
+    <p>{{$data->description}}</p>
+    <p>
+        <iframe src="{{url('storage/' .$data->file)}}" style="width: 600px;
+        height: 500px;"></iframe>
+    </p>
+    <button class="btn btn-dark"><a href="/files" style="color: white">Zurück</a></button>
 </div>
 @endsection
 
