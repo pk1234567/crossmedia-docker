37ccb3fc3235efb343e1016a72f12eeca5caa4f3
Marius <marius.staud@icloud.com>
2021-02-09 12:03:55 +0100

    Vorschau-Seite angepasst

--- src/resources/views/document/details.blade.php
+++ src/resources/views/document/details.blade.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Details</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
+<div class= "col-md-9 col-lg-9 pull-right">
 <h2>{{$data->title}}</h2>
-<h3>{{$data->description}}</h3>
+<p>{{$data->description}}</p>
 <p>
     <iframe src="{{url('storage/' .$data->file)}}" style="width: 600px;
     height: 500px;"></iframe>
-
 </p>
-</body>
-</html>
+<button class="btn btn-dark"><a href="/files" style="color: white">Zurück</a></button>
+</div>
+@endsection
+
